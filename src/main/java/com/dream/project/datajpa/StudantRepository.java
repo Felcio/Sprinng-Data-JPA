a2b549a1e3b470e72805de56713894dcae674fbd
Felcio <felcioedmundo@mail.com>
2021-05-17 02:25:17 +0200

    Gravando Dados

---
+++ src/main/java/com/dream/project/datajpa/StudantRepository.java
@@ -0,0 +1,8 @@
+package com.dream.project.datajpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudantRepository extends JpaRepository<Student, Long> {
+
+
+}
